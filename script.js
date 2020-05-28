c744efd185398ae83c0c7c00092a73a991372ea7
cristiantoong <cristiantoong@gmail.com>
2020-05-29 06:47:53 +0800

    added section

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const mobileMenu = document.getElementById('mobile-menu');
 const exitBtn = document.getElementById('exit-btn');
 
 menuBtn.addEventListener('click', function(){
-    mobileMenu.style.visibility = 'unset';
+    // mobileMenu.style.visibility = 'unset';
+    mobileMenu.style.transform = 'translateX(0%)';
+    mobileMenu.style.transition = 'transform 0.3s ease-in';
 });
 
 exitBtn.addEventListener('click', function(){
-    mobileMenu.style.visibility = 'hidden';
+    // mobileMenu.style.visibility = 'hidden';
+    mobileMenu.style.transform = 'translateX(100%)';
+    mobileMenu.style.transition = 'transform 0.3s ease-in';
 });
